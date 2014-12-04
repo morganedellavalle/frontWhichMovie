7694b330fbfd54ee848aea5f643c48b4e9535410
emericfossorier <Emeric@MacBook-Pro-de-Emeric.local>
2014-12-04 15:48:47 +0100

    BO

--- frontWhichMovie/platforms/ios/www/js/app.js
+++ frontWhichMovie/platforms/ios/www/js/app.js
@@ -1,15 +1,12 @@
-// We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
 (function () {
 
-    console.log("Coucou from app.js");
     HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
     FilmListView.prototype.template =
             Handlebars.compile($("#film-list-tpl").html());
+    FilmView.prototype.template = Handlebars.compile($("#film-tpl").html());
+    Liked.prototype.template = Handlebars.compile($("#liked-tpl").html());
     /* ---------------------------------- Local Variables ---------------------------------- */
-
-    //var filmListTpl = Handlebars.compile($("#film-list-tpl").html());
     var service = new FilmService();
-    //var films;
 
 
     service.initialize().done(function () {
@@ -26,48 +23,28 @@
                 $('body').html(new FilmListView(films).render().$el);
             });
         });
-        // router.addRoute( '', function() {
-        //     service.findAll().done(function() {
-        //         $('body').html(new FilmListView(service).render().$el);
-        //      });
-        // });
-        //service.findAll().done(function(data) {
-            //films = data;
-            //console.log("films :", films);
-            //console.log("show you are here");
-        //});
-    //    renderHomeView();
+
+               // Route to the view of a unique film
+        router.addRoute('films/:id', function(id) {
+            service.findById(parseInt(id)).done(function(film) {
+            $('body').html(new FilmView(film).render().$el);
+            });
+        });
+
+        // Route to a liked movie
+        router.addRoute('liked', function() {
+            service.done(function() {
+            $('body').html(new Liked().render().$el);
+            });
+        });
+
         router.start();
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
-    // $('.search-key').on('keyup', findByName);
-    // $('.help-btn').on('click', function() {
-    //     alert("Employee Directory v3.4");
-    // });
-
-
-    // $('.btn-primary').on('click', function() {
-    //     console.log("coucou");
-    //     $('body').html(new FilmListView(service).render().$el);
-    // });
 
     FastClick.attach(document.body);
     /* ---------------------------------- Local Functions ---------------------------------- */
-    // function renderHomeView() {
-    // $('body').html(homeTpl());
-    // //$('.search-key').on('keyup', findByName);
-    // }
-    // function findByName() {
-    //     service.findByName($('.search-key').val()).done(function (employees) {
-    //         var l = employees.length;
-    //         var e;
-    //         $('.employee-list').empty();
-    //         for (var i = 0; i < l; i++) {
-    //             e = employees[i];
-    //             $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-    //         }
-    //     });
-    // }
+
 
 }());
