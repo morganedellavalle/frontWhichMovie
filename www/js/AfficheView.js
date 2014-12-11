233810f5ac2667cf80f71e4788d53f4a266fc649
Guillaume Jouffre <guillaume.jouffre19@gmail.com>
2014-12-11 02:06:14 +0100

    le matchhhhhhhhhhhh

--- www/js/AfficheView.js
+++ www/js/AfficheView.js
@@ -13,22 +13,37 @@ var AfficheView = function (film) {
       this.$el.html(this.template(film));
       return this;
     };
-
     this.likeswipe = function(){
-    $(this).addClass('rotate-left').delay(800).fadeOut(800);
-    $('.affiche').find('.status').remove();
-    $(this).append('<div class="like">Like!</div>');
-    window.localStorage.setItem('film', film.id);
-    console.log(film.id);
-    console.log(localStorage.getItem("user_name"));
-    var x= Number(film.id)+1;
-    setTimeout(function() {
-        router.load("films/"+x+"/affiche");
-        }, 800);
-    var params = {"like": {"user_name":localStorage.getItem("user_name")}};
-    $.post("http://whichmovie.herokuapp.com/films/" + film.id + "/like.json", params)
+        $(this).addClass('rotate-left').delay(800).fadeOut(800);
+        $('.affiche').find('.status').remove();
+        $(this).append('<div class="like">Like!</div>');
+        window.localStorage.setItem('film', film.id);
+        var x= Number(film.id)+1;
+        setTimeout(function() {
+            router.load("films/"+x+"/affiche");
+            }, 800);
+        var params = {"like": {"user_name":localStorage.getItem("user_name")}};
+        var buddy
+
+        $.post("http://localhost:3000/films/" + film.id + "/like.json", params, function(response){
+        //console.log(Number(response.number)-1);
+            var array = $.map(response.other_likes, function(value, index) {
+                    return [value];
+            });
+            for (i = 0; i < array.length; i++) {
+                console.log(array[i].user_name);
+                console.log(localStorage.getItem("friend_1"));
+                if (array[i].user_name==localStorage.getItem("friend_1")){
+                        alert("There is a match with "+ array[i].user_name);
+                };
+           
+                if (array[i].user_name==localStorage.getItem("friend_2")){
+                        alert("There is a match with "+ array[i].user_name);
+                };            
+            };
+            
+        });
     };
-    
 
 
 
@@ -56,7 +71,24 @@ var AfficheView = function (film) {
     //alert("Liked");
     router.load("films/"+x+"/affiche");
     var params = {"like": {"user_name":localStorage.getItem("user_name")}};
-    $.post(" http://whichmovie.herokuapp.com/films/" + film.id + "/like.json", params)
+    $.post("http://localhost:3000/films/" + film.id + "/like.json", params, function(response){
+    //console.log(Number(response.number)-1);
+        var array = $.map(response.other_likes, function(value, index) {
+                return [value];
+        });
+        for (i = 0; i < array.length; i++) {
+            console.log(array[i].user_name);
+            console.log(localStorage.getItem("friend_1"));
+            if (array[i].user_name==localStorage.getItem("friend_1")){
+                    alert("There is a match with "+ array[i].user_name);
+            };
+       
+            if (array[i].user_name==localStorage.getItem("friend_2")){
+                    alert("There is a match with "+ array[i].user_name);
+            };            
+        };
+        
+    });
     };
 
 
